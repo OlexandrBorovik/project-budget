6b63375a052f6608b873f1354147a495cbe66e37
OlexandrBorovik <100314860+OlexandrBorovik@users.noreply.github.com>
2022-05-30 20:03:46 +0300

    Update Bill.java

--- Bill.java
+++ Bill.java
@@ -1,5 +1,7 @@
-package bills;
+package atlas;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,9 +13,8 @@ public class Bill {
 	private Map<String, Integer> exspens = new HashMap<>();
 	private double billProfit;
 	private double billExspens;
-	
-	
-	
+	final WriteText wt = new WriteText();
+
 	public Bill(Map<String, Integer> profit, Map<String, Integer> exspens, double billProfit, double billExspens) {
 		super();
 		this.profit = profit;
@@ -56,71 +57,77 @@ public class Bill {
 
 	public Bill() {
 		super();
-		// TODO Auto-generated constructor stub
+
 	}
 
-	public void addMoney(int changeSum, String category) {
-		this.setBillProfit(this.getBillProfit() + changeSum); 
-       // Check category exist
+	public void addMoney(int changeSum, String category) throws FileNotFoundException, IOException {
+
+		this.setBillProfit(this.getBillProfit() + changeSum);
+		// Check category exist
 		if (profit.containsKey(category)) {
 			int s = profit.get(category);
 			s = s + changeSum;
 			profit.replace(category, s);
+			String forFile = category + ";" + changeSum;
+			wt.writer(forFile);
 			System.out.println(changeSum + "UAH earned");
 
 		} else {
-			//add new category
+			// add new category
 			profit.put(category, changeSum);
-
+			String forFile = category + ";" + changeSum;
+			wt.writer(forFile);
 			System.out.println(changeSum + "UAH earned");
 		}
-		
 
-	}  
-	
-	public void remMoney(int changeSum, String category) {
-		
-		this.setBillExspens(this.getBillExspens() + changeSum); 
-       // Check category exist
+	}
+
+	public void remMoney(int changeSum, String category) throws FileNotFoundException, IOException {
+
+		this.setBillExspens(this.getBillExspens() + changeSum);
+		// Check category exist
 		if (exspens.containsKey(category)) {
 			int s = exspens.get(category);
 			s = s + changeSum;
 			exspens.replace(category, s);
 			System.out.println(changeSum + "UAH spent");
+			String forFile = "del;" + category + ";" + changeSum;
+			wt.writer(forFile);
 
 		} else {
-			//add new category
+			// add new category
 			exspens.put(category, changeSum);
-
+			String forFile = "del;" + category + ";" + changeSum;
+			wt.writer(forFile);
 			System.out.println(changeSum + "UAH spent");
 		}
-		
 
 	}
-	public  void persentCategoryAdd() {
-		Set<Map.Entry<String, Integer>> hms=profit.entrySet();
-		Set<Map.Entry<String, Integer>> sp=exspens.entrySet();
+
+	public void persentCategoryAdd() {
+		Set<Map.Entry<String, Integer>> hms = profit.entrySet();
+		Set<Map.Entry<String, Integer>> sp = exspens.entrySet();
 		System.out.println("_____PROFIT_____");
-		for(Map.Entry<String, Integer> hmse:hms){
-			
-			System.out.print(hmse.getValue()+" UAH ");
-			double a = (double)hmse.getValue() ;
-			double per =(a/billProfit)*100;
+		for (Map.Entry<String, Integer> hmse : hms) {
+
+			System.out.print(hmse.getValue() + " UAH ");
+			double a = (double) hmse.getValue();
+			double per = (a / billProfit) * 100;
 			String formattedDouble = new DecimalFormat("#0.00").format(per);
-			System.out.println(hmse.getKey() + " - "+formattedDouble + '%');
-			
+			System.out.println(hmse.getKey() + " - " + formattedDouble + '%');
+
 		}
 		System.out.println("_____EXSPENS_____");
-		for(Map.Entry<String, Integer> hmse:sp){
-			System.out.print(hmse.getValue()+" UAH ");
-			double b = (double)hmse.getValue() ;
-			double per =(b/billExspens)*100;
+		for (Map.Entry<String, Integer> hmse : sp) {
+			System.out.print(hmse.getValue() + " UAH ");
+			double b = (double) hmse.getValue();
+			double per = (b / billExspens) * 100;
 			String formattedDouble = new DecimalFormat("#0.00").format(per);
-			System.out.println(hmse.getKey() +" - "+ formattedDouble + '%');
-		
+			System.out.println(hmse.getKey() + " - " + formattedDouble + '%');
 
-	} 	System.out.println("_____YOU HAVE_____");
-		System.out.println(billProfit-billExspens + "UAH.");
+		}
+		System.out.println("_____YOU HAVE_____");
+		System.out.println(billProfit - billExspens + "UAH.");
 
 	}
 
@@ -128,5 +135,5 @@ public class Bill {
 	public String toString() {
 		return "Bill [profit=" + profit + ", exspens=" + exspens + ", billProfit=" + billProfit + ", billExspens="
 				+ billExspens + "]";
-	}	   
+	}
 }
